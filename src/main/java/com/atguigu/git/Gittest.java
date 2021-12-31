815814aefb9a0a7ced292a529d1f2c802a2c3336
zhou <zhou@git.com>
2021-12-31 23:52:27 +0800

    hot fox commit1

--- src/main/java/com/atguigu/git/Gittest.java
+++ src/main/java/com/atguigu/git/Gittest.java
@@ -5,5 +5,6 @@ public class Gittest {
         System.out.println("hello git!");
         System.out.println("git modify");
         System.out.println("third");
+        System.out.println("hot fix modi");
     }
 }
