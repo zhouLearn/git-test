d20339099f72eec48deaf2ee82f5c7bb54a82da7
zhou <zhou@git.com>
2021-12-31 23:35:22 +0800

    third commit

--- src/main/java/com/atguigu/git/Gittest.java
+++ src/main/java/com/atguigu/git/Gittest.java
@@ -4,5 +4,6 @@ public class Gittest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("git modify");
+        System.out.println("third");
     }
 }
